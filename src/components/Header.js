456d16eca23736f6c60e869d695fa1744d4389cd
EsraaShurafat <esraahammad93@gmail.com>
2021-10-05 03:45:06 +0300

    frist section

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './Header.css';
+
+class Header extends React.Component{
+    render(){
+        return(<>
+        <div className="div1"></div>
+
+        </>)
+    }
+}
+
+export default Header;
---
+++ src/components/Main.js
@@ -0,0 +1,18 @@
+import React from "react";
+import './Main.css';
+class Main extends React.Component{
+    render(){
+        return(<>
+        <div className="main">
+            <h1>I'm Esra'a Alshurafat</h1>
+            <p className="bio">A passionate Full Stack Web Developer and having an experience of building Web applications with JavaScript / Reactjs/HTML/CSS/Bootstrap , and some other cool libraries and frameworks. </p>
+
+            <div><img src="https://avatars.githubusercontent.com/u/85869661?v=4" id="myImg"/></div>
+            <div className="main2"></div>
+        </div>
+       
+        </>)
+    }
+}
+
+export default Main;
